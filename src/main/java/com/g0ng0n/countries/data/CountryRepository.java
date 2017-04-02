9229d35f4c7d806045cb9d8fa9ba3d70f6184ca9
Gonzalo Gisbert <gonzalo.a.gisbert@gmail.com>
2017-04-02 02:44:53 -0300

    added the Country Model

---
+++ src/main/java/com/g0ng0n/countries/data/CountryRepository.java
@@ -0,0 +1,16 @@
+package com.g0ng0n.countries.data;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * Created by g0ng0n.
+ */
+@Component
+public class CountryRepository {
+
+
+    public List<Country> getAllCountries() {
+    }
+}
