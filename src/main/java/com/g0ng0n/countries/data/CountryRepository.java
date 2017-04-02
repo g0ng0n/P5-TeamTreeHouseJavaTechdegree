5c387f890047e147d93cee0ece990808e1b194b4
Gonzalo Gisbert <gonzalo.a.gisbert@gmail.com>
2017-04-02 02:59:20 -0300

    added the Countries information and the getAllCountries functionality with that information

--- src/main/java/com/g0ng0n/countries/data/CountryRepository.java
+++ src/main/java/com/g0ng0n/countries/data/CountryRepository.java
@@ -1,7 +1,9 @@
 package com.g0ng0n.countries.data;
 
+import com.g0ng0n.countries.model.Country;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -10,7 +12,17 @@ import java.util.List;
 @Component
 public class CountryRepository {
 
+    private static final List<Country> ALL_COUNTRIES = Arrays.asList(
+            new Country(1, "Ecuador","15.74 million", "Quito", Arrays.asList("Spanish","Kichwa","Shuar"),"/resources/flags/ecuador.png"),
+            new Country(2,"France","66.03 million","Paris",Arrays.asList("French"),"/resources/flags/paris.png"),
+            new Country(3,"India","1.252 billion","New Delhi",Arrays.asList("Hindi","English"),"/resources/flags/india.png"),
+            new Country(4,"Italy","59.83 million","Rome",Arrays.asList("Italian"),"/resources/flags/italy.png"),
+            new Country(5,"Kenya","44.35 million","Nairobi",Arrays.asList("English","Kiswahili"),"/resources/flags/kenya.png")
 
-    public List<Country> getAllCountries() {
+    );
+
+    public List<Country> getAllCountries(){
+        return ALL_COUNTRIES;
     }
+
 }
