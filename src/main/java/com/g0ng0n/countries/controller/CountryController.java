59c89b9a876c674e15fb6d6b161b1d0f05fd79b5
Gonzalo Gisbert <gonzalo.a.gisbert@gmail.com>
2017-04-03 22:01:10 -0300

    added minor fixes to the controller

--- src/main/java/com/g0ng0n/countries/controller/CountryController.java
+++ src/main/java/com/g0ng0n/countries/controller/CountryController.java
@@ -5,6 +5,7 @@ import com.g0ng0n.countries.model.Country;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -24,4 +25,23 @@ public class CountryController {
         modelMap.put("countries", list);
         return "countries";
     }
+
+    @RequestMapping("/countries/{id}")
+    public String getCountryById(@PathVariable int id, ModelMap modelMap){
+        Country country = countryRepository.findById(id);
+        modelMap.put("country", country);
+
+        return "country";
+    }
+
+
+    public CountryRepository getCountryRepository() {
+        return countryRepository;
+    }
+
+    public void setCountryRepository(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+
 }
